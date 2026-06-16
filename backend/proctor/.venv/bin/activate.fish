c46ce4a607d100e4503feec22b4d1ff8b98c7d33
Sanjay S <sanjaysha9468@gmail.com>
2026-06-16 18:18:32 +0530

    Update production API URLs

--- backend/proctor/.venv/bin/activate.fish
+++ backend/proctor/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sanjay/Projects/Lab-Record-system/backend/proctor/.venv"
+set -gx VIRTUAL_ENV "/Users/sanjay/Library/Mobile Documents/com~apple~CloudDocs/Projects/Lab-Record-system/backend/proctor/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
